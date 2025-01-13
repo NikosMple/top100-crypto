676174b0e8530fcf823f4cacb850390aeb75d20e
nikos <nikosmple1998@gmail.com>
2025-01-13 16:41:15 +0200

    Update package.json with start script for Render deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import env from "dotenv";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 env.config();
 
 const APIKey = process.env.API_KEY;
